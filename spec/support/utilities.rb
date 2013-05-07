1292bd24c20ebcc52ba84232aed453ed5cae0719
Shiv Kumar <shivpkumar@gmail.com>
2013-05-07 15:43:11 -0500

    Finish user edit, update, index and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,41 @@
 include ApplicationHelper
+
+RSpec::Matchers.define :have_in_header do |message|
+  match do |page|
+    page.should have_selector('h1', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_in_title do |message|
+  match do |page|
+    page.should have_selector('title', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
+def valid_info
+	fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
